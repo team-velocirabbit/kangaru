460ce4f46502c6b6e767d4db25065b1e2386af92
Josie Glore <josieglore@gmail.com>
2018-12-25 22:14:01 -0800

    add initial options logic in jobs container

--- containers/jobs.js
+++ containers/jobs.js
@@ -1,23 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Extract from './extract';
 import Transform from '../components/transform';
 import Load from './load';
+import Options from '../components/Options';
 
 class Jobs extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      emailCheck: false,
+      email: '',
+      textCheck: false,
+      phoneNumber: '',
     }
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePhoneChange = this.handlePhoneChange.bind(this);
+    this.handleSelection = this.handleSelection.bind(this);
   }
+
+  handleEmailChange(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+
+  handlePhoneChange(e) {
+    this.setState({
+      phoneNumber: e.target.value,
+    });
+  }
+
+  handleSelection(e) {
+    console.log('e value is ', e.target.value);
+  }
+
   render() {
-    return(
-        <div>
-            <h1>Jobs Container</h1>
-            <Extract />
-            <Transform />
-            <Load />
-        </div>
+    const { emailCheck, textCheck, email, phoneNumber } = this.state;
+    return (
+      <div>
+        <h1>Jobs Container</h1>
+        <Extract />
+        <Transform />
+        <Load />
+        <Options 
+          emailCheck = {emailCheck}
+          textCheck = {textCheck}
+          email = {email}
+          phoneNumber = {phoneNumber}
+          handleSelection = {this.handleSelection}
+        />
+      </div>
     );
   };
 };
