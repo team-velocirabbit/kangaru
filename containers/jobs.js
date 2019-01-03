809992bd568e345d232867ac6fe2fe32299c3ca8
jaelee213 <jaelee213@gmail.com>
2019-01-02 17:50:56 -0800

    added ipc

--- containers/jobs.js
+++ containers/jobs.js
@@ -5,6 +5,10 @@ import Load from './load';
 import Options from '../components/options';
 import '../main.css';
 
+
+import { ipcRenderer } from 'electron';
+
+
 const sgMail = require('@sendgrid/mail');
 const etl = require('etl-test');
 const remote = require('electron').remote;
@@ -52,6 +56,13 @@ class Jobs extends Component {
     this.browseFiles = this.browseFiles.bind(this);
     this.browseDirectories = this.browseDirectories.bind(this);
     this.startEtl = this.startEtl.bind(this);
+  }
+
+  componentDidMount() {
+    ipcRenderer.on('notify', (event, args) => {
+      console.log('gonna notify')
+      this.handleNotifications();
+    });
   }
 
   handleEmailChange(e) {
@@ -194,40 +205,26 @@ class Jobs extends Component {
     }
 
     startEtl() {
+      const startObject = {
+        name: this.props.name,
+        start: 'n/a',
+        end: 'n/a',
+        status: 'in progress...'
+      }
+
       const { extractUri, loadUri, filePath, fileName, script } = this.state;
-      const newScript = script.substring(script.indexOf('{') + 1, script.lastIndexOf('}'));
-      const scriptFunc = new Function('data', newScript);
-           
-      if (extractUri.length > 0) {
-        if (loadUri.length > 0) {
-          new etl()
-          .simple(extractUri, scriptFunc, loadUri, 'my_database')
-          .combine()
-          .start()
-        }
-        else {
-          new etl()
-          .simple(extractUri, scriptFunc, fileName, 'my_database')
-          .combine()
-          .start()
-        }
+      
+      const etlObject = {
+        name: this.props.name,
+        extractUri,
+        loadUri,
+        filePath,
+        fileName,
+        script
       }
-      if (filePath.length > 0) {
-        if (loadUri.length > 0) {
-          new etl()
-          .simple(filePath, scriptFunc, loadUri, 'my_database')
-          .combine()
-          .start()
-        }
-        else {
-          new etl()
-          .simple(filePath, scriptFunc, fileName, 'my_database')
-          .combine()
-          .start()
-        }
-      }
-      // send notifications
-      this.handleNotifications();
+      
+      ipcRenderer.send('start', startObject);
+      ipcRenderer.send('etl', etlObject);  
     }
 
   render() {
